a8fe3e638a7d257b095071ad9c817e81c18a113c
gbade francis <gbade.francis@gmail.com>
2022-05-07 20:32:47 +0100

    Dependencies installed such as axios, react boostrap, Bootstrap and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './includes/header';
+
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+
       <p>This ny aplication</p>
+      <Button>submit</Button>
+      
     </div>
   );
 }
