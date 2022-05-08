11857d01a115d57c29be257a543fa318f584c5a8
gbade francis <gbade.francis@gmail.com>
2022-05-08 02:41:37 +0100

    Users login code sucessfully added. This is been done with authentication for each field then user will be directed to Users Dashboard after a successful Login

--- src/includes/header.js
+++ src/includes/header.js
@@ -3,6 +3,9 @@ import { Navbar,Container,Nav,NavDropdown, Form,FormControl,Button } from 'react
 import logow from "../images/logow.png"
 import { useState } from 'react';
 import {Modal} from "react-bootstrap"
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { setUserSession } from '../Session/userSession';
 
 
 
@@ -10,6 +13,47 @@ const Header = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+
+
+  const userLogin = {
+    email: "",
+    password: "",
+}; 
+const [user, setUser] = useState(userLogin)
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+}
+
+  
+
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios.post('user/userlogin', user).then(response => {
+        if (response.data.message == "You have successfully Logged in") {
+            console.log(response.data);
+            alert("You have Successfully Logged in");
+            setUserSession(response.data.token);
+            handleClose()
+            navigate('/dashboard/Index');
+        }
+
+    })
+        .catch(error => {
+            if (error.response.message === "Invaid Credentials" || error.response.status === 401) {
+                alert("Wrong Username and Password");
+            } else if(error.response.message === "Invalid Email Name" || error.response.status === 401) {
+                alert("Invalid Credentials");
+                navigate("index")
+            }else if(error.response.message === "Empty Credentials Supplied!" || error.response.status === 500){
+              alert("Empty Credentials");
+            }
+        })
+}
+
     return (
         <div>
             <Navbar bg="dark" variant="dark" expand="lg" className='fixed-top'>
@@ -38,11 +82,11 @@ const Header = () => {
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" autoFocus /></Form.Group>
+                        <Form.Control type="email" name="email" placeholder="name@example.com" autoFocus onChange={handleInputChange} /></Form.Group>
                         
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="***********" autoFocus /></Form.Group>
+                        <Form.Control type="password" name="password" placeholder="***********" autoFocus onChange={handleInputChange}  autoFocus  /></Form.Group>
 
                         
                     </Form>
@@ -51,7 +95,7 @@ const Header = () => {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={handleLogin}>
                         Login
                     </Button>
                     </Modal.Footer>
