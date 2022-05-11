26e9d91ad86d1090131c70e5f8c723ba2cfa5ab0
gbade francis <gbade.francis@gmail.com>
2022-05-11 02:01:48 +0100

    Logout added successfully

--- src/dashboard/home.js
+++ src/dashboard/home.js
@@ -13,7 +13,29 @@ import { removeUserSession } from '../Session/userSession';
 const Home = () => {
     const getToken = localStorage.getItem("token");
     const navigate = useNavigate();
-    
+    const handleLogout = async () => {
+        
+        
+        await axios.post(`user/Logout`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${getToken}`
+            },
+        }).then(function (response) {
+            console.log(response);
+            if (response.data.message == "Logout") {
+                new Swal("You Have Successfully Logged Out!!!");
+                removeUserSession("data", "token");
+                /* history.push("/"); */
+                navigate('/');
+
+            } else {
+                alert("Error Logout");
+            }
+        })
+
+    };
     return (
         <>
         <div className='dashboard-header justify-content-between d-flex p-2'>
